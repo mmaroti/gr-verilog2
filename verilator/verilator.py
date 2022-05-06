c2e4497e587bfde5f60e644de7b7f4d933ea5931
Miklos Maroti <mmaroti@gmail.com>
2022-05-06 00:20:36 -0500

    almost complete rewrite of verilator code

--- verilator/verilator.py
+++ verilator/verilator.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from typing import Any, Dict, Optional
+from typing import Any, Callable, Dict, List, Optional
 
 import ctypes
 import hashlib
@@ -22,62 +22,84 @@ import json
 import os
 import re
 import subprocess
+import threading
 
 
-class Verilator:
-    def __init__(self,
-                 verilog: str,
-                 params: Dict[str, Any],
-                 build_path: Optional[str] = None):
-        self.verilog = os.path.abspath(verilog)
-        if not os.path.exists(verilog):
-            raise ValueError('Verilog file not found')
+class Module:
+    def __init__(self, sources: List[str],
+                 component: Optional[str] = None,
+                 build_dir: Optional[str] = None):
+        assert len(sources) >= 1
+        sources = [os.path.abspath(s) for s in sources]
+        for s in sources:
+            assert os.path.exists(s)
+        self.sources = sources
 
-        self.component = os.path.splitext(os.path.basename(self.verilog))[0]
-        self.params = params
+        if component is None:
+            component = os.path.splitext(os.path.basename(sources[0]))[0]
+        self.component = component
 
-        if build_path is None:
-            build_path = os.path.join(os.path.dirname(self.verilog), 'build')
-        self.build_path = build_path
-        if not os.path.exists(self.build_path):
-            os.mkdir(self.build_path)
+        if build_dir is None:
+            build_dir = os.path.join(os.path.dirname(sources[0]), 'build')
+        self.build_dir = build_dir
+        if not os.path.exists(build_dir):
+            os.mkdir(build_dir)
 
-        self.hash = hashlib.md5(str(params).encode('utf-8')).hexdigest()
+        self.ports_cache = dict()
+        self.lib_cache = dict()
 
-        self.obj_dir = 'verilator-' + self.hash
-        obj_path = os.path.join(self.build_path, self.obj_dir)
-        if not os.path.exists(obj_path):
-            os.mkdir(obj_path)
+    def _get_max_mtime(self):
+        """
+        Returns the maximum update time of all sources.
+        """
+        m = os.path.getmtime(__file__)
+        for s in self.sources:
+            m = max(m, os.path.getmtime(s))
+        return m
 
-        self.lib_name = 'lib' + self.component + '-' + self.hash + '.so'
+    _BUILD_LOCK = threading.Lock()
+    _BUILD_COND = dict()
 
-        lib_path = os.path.join(self.build_path, self.obj_dir, self.lib_name)
-        if os.path.exists(lib_path):
-            mtime = max([os.path.getmtime(f) for f in [
-                __file__,
-                verilog,
-            ]])
-            if os.path.getmtime(lib_path) < mtime:
-                os.remove(lib_path)
+    @staticmethod
+    def _build_job(key: str, job: Callable) -> Any:
+        """
+        This method calls the given job with the arguments, but makes sure
+        that for any key only one job is running at any given time.
+        """
 
-        if not os.path.exists(lib_path):
-            self._generate_verilator()
-            self._generate_wrapper()
-            self._make()
+        with Module._BUILD_LOCK:
+            while True:
+                if key in Module._BUILD_COND:
+                    Module._BUILD_COND[key].wait()
+                else:
+                    cond = threading.Condition(Module._BUILD_LOCK)
+                    Module._BUILD_COND[key] = cond
+                    break
 
-        self.lib = ctypes.cdll.LoadLibrary(lib_path)
-        self.lib.create_block.restype = ctypes.c_void_p
-        self.lib.destroy_block.argtypes = [ctypes.c_void_p]
-        self.lib.reset_block.argtypes = [ctypes.c_void_p]
-        self.lib.block_config.argtypes = [ctypes.c_void_p]
-        self.lib.block_config.restype = ctypes.c_wchar_p
-        self.lib.library_config.restype = ctypes.c_wchar_p
+        ret = job()
 
-    def _generate_verilator(self):
-        obj_path = os.path.join(self.build_path, self.obj_dir)
-        if os.path.exists(obj_path):
-            for file in os.listdir(obj_path):
-                os.remove(os.path.join(obj_path, file))
+        with Module._BUILD_LOCK:
+            del Module._BUILD_COND[key]
+            cond.notifyAll()
+
+        return ret
+
+    def _get_obj_dir(self, params: Dict[str, Any]) -> str:
+        hash = hashlib.md5(str(params).encode('utf-8')).hexdigest()
+        return os.path.join(self.build_dir, 'verilator-' + hash)
+
+    def _verilator_job(self, params: Dict[str, Any]):
+        obj_dir = self._get_obj_dir(params)
+
+        if not os.path.exists(obj_dir):
+            os.mkdir(obj_dir)
+
+        header = os.path.join(obj_dir, self.component + '.h')
+        if os.path.exists(header):
+            if self._get_max_mtime() <= os.path.getmtime(header):
+                return
+            for f in os.listdir(obj_dir):
+                os.remove(os.path.join(obj_dir, f))
 
         command = [
             'verilator',
@@ -87,21 +109,30 @@ class Verilator:
             '-CFLAGS', '-fPIC',
             '-LDFLAGS', '-shared',
             '--prefix', self.component,
-            '--Mdir', self.obj_dir,
-            '-o', self.lib_name,
-            self.verilog,
-            'wrapper.cpp',
+            '--Mdir', obj_dir,
+            '-o', 'lib{}.so'.format(self.component),
         ]
         command.extend(['-G{}={}'.format(key, str(val))
-                        for key, val in self.params.items()])
+                        for key, val in params.items()])
+        command.extend(self.sources)
+        command.append('wrapper.cpp')
 
         print(" ".join(command))
-        result = subprocess.run(command, cwd=self.build_path)
+        result = subprocess.run(command, cwd=self.build_dir)
         result.check_returncode()
+        assert(os.path.exists(header))
 
     _RE_PORT = re.compile(r'^\s*VL_(IN|OUT)(|8|16|32|64)\((\w+),(\d+),(\d+)\)')
 
-    def _parse_ports(self) -> Dict[str, Any]:
+    def _parse_ports_job(self, obj_dir: str) -> Dict[str, Any]:
+        header_path = os.path.join(obj_dir, self.component + '.h')
+
+        mtime = os.path.getmtime(header_path)
+        if header_path in self.ports_cache:
+            ports, mtime2 = self.ports_cache[header_path]
+            if mtime == mtime2:
+                return ports
+
         clocks = []
         resets = []
         buses = {}
@@ -116,7 +147,7 @@ class Verilator:
                     assert width == 1
 
                 if sig == 'tready':
-                    dir = 'IN' if dir != 'IN' else 'OUT'
+                    dir = 'OUT' if dir == 'IN' else 'IN'
 
                 axis = buses.setdefault(bus, {'dir': dir})
                 assert axis['dir'] == dir
@@ -126,11 +157,9 @@ class Verilator:
                 return True
             return False
 
-        filename = os.path.join(
-            self.build_path, self.obj_dir, self.component + '.h')
-        with open(filename, 'r') as file:
+        with open(header_path, 'r') as file:
             for line in file.readlines():
-                match = Verilator._RE_PORT.match(line)
+                match = Module._RE_PORT.match(line)
                 if not match:
                     continue
 
@@ -150,8 +179,8 @@ class Verilator:
                 else:
                     raise ValueError('invalid signal: ' + name)
 
-        for axis in buses.values():
-            assert 'tvalid' in axis and 'tready' in axis
+        for bus in buses.values():
+            assert 'tvalid' in bus and 'tready' in bus
 
         inputs = [{
             'name': key,
@@ -167,19 +196,36 @@ class Verilator:
             'tlast': val.get('tlast', 0),
         } for key, val in buses.items() if val['dir'] != 'IN']
 
-        return {
+        ports = {
             'clocks': sorted(clocks),
             'resets': sorted(resets),
             'inputs': sorted(inputs, key=lambda d: d['name']),
             'outputs': sorted(outputs, key=lambda d: d['name']),
         }
+        self.ports_cache[header_path] = (ports, mtime)
+
+        return ports
+
+    def get_ports(self, params: Dict[str, Any]):
+        obj_dir = self._get_obj_dir(params)
+
+        def job():
+            self._verilator_job(params)
+            return self._parse_ports_job(obj_dir)
+
+        return Module._build_job(obj_dir, job)
 
     _WRAPPER = """// Generated, do not modify!
 
-#include <iostream>
-#include "{component}.h"
+# include <iostream>
+# include "{component}.h"
 
 const wchar_t *CONFIG = L"{config}";
+
+extern "C" const wchar_t *config()
+{{
+    return CONFIG;
+}}
 
 struct Block
 {{
@@ -201,16 +247,6 @@ extern "C" void destroy_block(Block *block)
     delete block;
 }}
 
-extern "C" const wchar_t *library_config()
-{{
-    return CONFIG;
-}}
-
-extern "C" const wchar_t *block_config(Block *block)
-{{
-    return block->config;
-}}
-
 void set_clocks(Block *block, int value)
 {{
 {set_clocks}}}
@@ -221,6 +257,7 @@ void set_resets(Block *block, int value)
 
 extern "C" void reset_block(Block *block)
 {{
+    assert(block != nullptr);
     set_resets(block, 1);
 {inhibits}
     for (int i = 1; i <= 4; i++)
@@ -233,8 +270,16 @@ extern "C" void reset_block(Block *block)
 }}
 """
 
-    def _generate_wrapper(self):
-        ports = self._parse_ports()
+    def _compile_job(self, params: Dict[str, Any]):
+        obj_dir = self._get_obj_dir(params)
+
+        lib_path = os.path.join(obj_dir, 'lib{}.so'.format(self.component))
+        if os.path.exists(lib_path):
+            if self._get_max_mtime() <= os.path.getmtime(lib_path):
+                return
+            os.remove(lib_path)
+
+        ports = self._parse_ports_job(obj_dir)
 
         set_clocks = ""
         for name in ports['clocks']:
@@ -252,14 +297,13 @@ extern "C" void reset_block(Block *block)
 
         config = {
             'component': self.component,
-            'params': self.params,
-            'hash': self.hash,
+            'params': params,
         }
         config.update(ports)
         config = json.dumps(config, ensure_ascii=True)
         config = config.replace('"', '\\"')
 
-        content = Verilator._WRAPPER.format(
+        content = Module._WRAPPER.format(
             component=self.component,
             config=config,
             set_clocks=set_clocks,
@@ -267,13 +311,10 @@ extern "C" void reset_block(Block *block)
             inhibits=inhibits,
         )
 
-        wrapper_path = os.path.join(
-            self.build_path, self.obj_dir, 'wrapper.cpp')
-
-        with open(wrapper_path, 'w') as file:
+        filename = os.path.join(obj_dir, 'wrapper.cpp')
+        with open(filename, 'w') as file:
             file.write(content)
 
-    def _make(self):
         command = [
             'make',
             '-j4',
@@ -282,8 +323,56 @@ extern "C" void reset_block(Block *block)
 
         print(" ".join(command))
         result = subprocess.run(
-            command, cwd=os.path.join(self.build_path, self.obj_dir))
+            command, cwd=os.path.join(obj_dir))
         result.check_returncode()
+        assert(os.path.exists(lib_path))
+
+    def _load_library_job(self, obj_dir: str) -> ctypes.CDLL:
+        lib_path = os.path.join(obj_dir, 'lib{}.so'.format(self.component))
+
+        mtime = os.path.getmtime(lib_path)
+        if lib_path in self.lib_cache:
+            lib, mtime2 = self.lib_cache[lib_path]
+            if mtime == mtime2:
+                return lib
+
+        lib = ctypes.cdll.LoadLibrary(lib_path)
+        lib.config.restype = ctypes.c_wchar_p
+        lib.create_block.restype = ctypes.c_void_p
+        lib.destroy_block.argtypes = [ctypes.c_void_p]
+        lib.reset_block.argtypes = [ctypes.c_void_p]
+        self.lib_cache[lib_path] = (lib, mtime)
+        return lib
+
+    def get_library(self, params: Dict[str, Any]):
+        obj_dir = self._get_obj_dir(params)
+
+        def job():
+            self._verilator_job(params)
+            self._compile_job(params)
+            return self._load_library_job(obj_dir)
+
+        return Module._build_job(obj_dir, job)
+
+
+class Instance:
+    def __init__(self, module: Module, params: Dict[str, Any]):
+        self.lib = module.get_library(params)
+        self.block = self.lib.create_block()
+
+    def close(self):
+        if self.block is not None:
+            self.lib.destroy_block(self.block)
+            self.block = None
+
+    def __del__(self):
+        self.close()
+
+    def get_config(self) -> Dict[str, Any]:
+        return json.loads(self.lib.config())
+
+    def reset(self):
+        self.lib.reset_block(self.block)
 
 
 LIBRARY = os.path.abspath(os.path.join(
@@ -291,15 +380,15 @@ LIBRARY = os.path.abspath(os.path.join(
 
 
 def test():
-    ver = Verilator(
+    mod = Module([
         os.path.join(LIBRARY, 'axis_copy_cdc', 'axis_copy_cdc.v'),
-        {'DATA_WIDTH': 32})
+    ])
 
-    print(ver.lib.library_config())
-    block = ver.lib.create_block()
-    ver.lib.reset_block(block)
-    print(ver.lib.block_config(block))
-    ver.lib.destroy_block(block)
+    ins1 = Instance(mod, {})
+    ins2 = Instance(mod, {'DATA_WIDTH': 32})
+
+    print(ins1.get_config())
+    print(ins2.get_config())
 
 
 if __name__ == '__main__':
